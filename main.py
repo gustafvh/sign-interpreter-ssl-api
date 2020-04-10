1a5072d0e8565bde84ac2c571a9ab502ff5d3e23
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-04-10 10:03:42 +0200

    deploy update

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def getTopPredictions(preds):
         'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0,
         'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0,
         'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0,
-        'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0, 'Å': 0,
-        'Ä': 0, 'Ö': 0,
+        'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0, 'A1': 0,
+        'A2': 0, 'O1': 0,
     }
     # map preds index with probability to correct letter from dictonary
     for i, key in enumerate(predsDict, start=0):
@@ -93,5 +93,5 @@ print("Loading Model...")
 load_model() #Load model before apps run to prevent long loading time
 print("Model Loaded. Starting Server...")
 
-if __name__ == "__main__":  #if running on development
-    app.run(threaded=False)
+# if __name__ == "__main__":  #if running on development
+#     app.run(threaded=False)
